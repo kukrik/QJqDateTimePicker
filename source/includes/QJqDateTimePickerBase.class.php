ca3734b20168cdfd53f6324e3a46fc45b2d1fe04
Vardan Akopian <vakopian@sustain.net>
2014-03-04 18:09:59 -0800

    upgrade to jquery-ui-timepicker-addon version 0.9.9 and make it compatible with qcubed v2.1

--- source/includes/QJqDateTimePickerBase.class.php
+++ source/includes/QJqDateTimePickerBase.class.php
@@ -1,353 +1,92 @@
 <?php
-	/* Custom event classes for this control */
+	class QJqDateTimePickerBase extends QJqDateTimePickerGen
+	{
+		protected $strDateFormat = 'MMM D, YYYY';
+		protected $strTimeFormat;
 
-	/**
-	 * @property boolean $AlwaysSetTime 
-	 * @property boolean $Ampm 
-	 * @property integer $Hour 
-	 * @property integer $HourMin 
-	 * @property integer $HourMax 
-	 * @property integer $Minute 
-	 * @property integer $MinuteMin 
-	 * @property integer $MinuteMax 
-	 * @property integer $Second 
-	 * @property integer $SecondMin 
-	 * @property integer $SecondMax 
-	 * @property boolean $ShowButtonPanel 
-	 * @property boolean $ShowHour 
-	 * @property boolean $ShowMinute 
-	 * @property boolean $ShowSecond 
-	 * @property boolean $ShowTime 
-	 * @property integer $StepHour 
-	 * @property integer $StepMinute 
-	 * @property integer $StepSecond 
-	 * @property string $JqTimeFormat 
-	 * @property boolean $TimeOnly 
-	 */
-
-	class QJqDateTimePickerBase extends QDatepickerBox	{
-		protected $strJavaScripts = __JQUERY_EFFECTS__;
-		protected $strStyleSheets = __JQUERY_CSS__;
-		/** @var boolean */
-		protected $blnAlwaysSetTime = null;
-		/** @var boolean */
-		protected $blnAmpm = null;
-		/** @var integer */
-		protected $intHour;
-		/** @var integer */
-		protected $intHourMin;
-		/** @var integer */
-		protected $intHourMax = null;
-		/** @var integer */
-		protected $intMinute;
-		/** @var integer */
-		protected $intMinuteMin;
-		/** @var integer */
-		protected $intMinuteMax = null;
-		/** @var integer */
-		protected $intSecond;
-		/** @var integer */
-		protected $intSecondMin;
-		/** @var integer */
-		protected $intSecondMax = null;
-		/** @var boolean */
-		protected $blnShowButtonPanel = null;
-		/** @var boolean */
-		protected $blnShowHour = null;
-		/** @var boolean */
-		protected $blnShowMinute = null;
-		/** @var boolean */
-		protected $blnShowSecond = null;
-		/** @var boolean */
-		protected $blnShowTime = null;
-		/** @var integer */
-		protected $intStepHour = null;
-		/** @var integer */
-		protected $intStepMinute = null;
-		/** @var integer */
-		protected $intStepSecond = null;
-		/** @var string */
-		protected $strJqTimeFormat;
-		/** @var boolean */
-		protected $blnTimeOnly = null;
-
-		/** @var array $custom_events Event Class Name => Event Property Name */
-		protected static $custom_events = array(
-		);
-		
 		public function  __construct($objParentObject, $strControlId = null) {
 			parent::__construct($objParentObject, $strControlId);
-			$this->AddPluginJavascriptFile("QJqDateTimePicker", "jquery-ui-timepicker-addon-0.6.2.min.js");
-			$this->AddPluginCssFile("QJqDateTimePicker", "jquery-ui-timepicker-addon-0.6.2.css");
+			$this->AddPluginJavascriptFile("QJqDateTimePicker", "jquery-ui-timepicker-addon-0.9.9.min.js");
+			$this->AddPluginCssFile("QJqDateTimePicker", "jquery-ui-timepicker-addon-0.9.9.css");
 		}
 
-		protected function makeJsProperty($strProp, $strKey) {
-			$objValue = $this->$strProp;
-			if (null === $objValue) {
-				return '';
+		static private $mapQC2JQ = array(
+			'hhhh' => 'hh',
+			'hh' => 'h',
+			'zz' => 'TT',
+			'z' => 'tt',
+			);
+		static private $mapJQ2QC = null;
+
+		static public function qcFrmt($jqFrmt) {
+			if (!self::$mapJQ2QC) {
+				self::$mapJQ2QC = array_flip(self::$mapQC2JQ);
 			}
-
-			return $strKey . ': ' . JavaScriptHelper::toJsObject($objValue) . ', ';
+			return strtr($jqFrmt, self::$mapJQ2QC);
 		}
 
-		protected function makeJqOptions() {
-			$strJqOptions = parent::makeJqOptions();
-			if ($strJqOptions) $strJqOptions .= ', ';
-			$strJqOptions .= $this->makeJsProperty('AlwaysSetTime', 'alwaysSetTime');
-			$strJqOptions .= $this->makeJsProperty('Ampm', 'ampm');
-			$strJqOptions .= $this->makeJsProperty('Hour', 'hour');
-			$strJqOptions .= $this->makeJsProperty('HourMin', 'hourMin');
-			$strJqOptions .= $this->makeJsProperty('HourMax', 'hourMax');
-			$strJqOptions .= $this->makeJsProperty('Minute', 'minute');
-			$strJqOptions .= $this->makeJsProperty('MinuteMin', 'minuteMin');
-			$strJqOptions .= $this->makeJsProperty('MinuteMax', 'minuteMax');
-			$strJqOptions .= $this->makeJsProperty('Second', 'second');
-			$strJqOptions .= $this->makeJsProperty('SecondMin', 'secondMin');
-			$strJqOptions .= $this->makeJsProperty('SecondMax', 'secondMax');
-			$strJqOptions .= $this->makeJsProperty('ShowButtonPanel', 'showButtonPanel');
-			$strJqOptions .= $this->makeJsProperty('ShowHour', 'showHour');
-			$strJqOptions .= $this->makeJsProperty('ShowMinute', 'showMinute');
-			$strJqOptions .= $this->makeJsProperty('ShowSecond', 'showSecond');
-			$strJqOptions .= $this->makeJsProperty('ShowTime', 'showTime');
-			$strJqOptions .= $this->makeJsProperty('StepHour', 'stepHour');
-			$strJqOptions .= $this->makeJsProperty('StepMinute', 'stepMinute');
-			$strJqOptions .= $this->makeJsProperty('StepSecond', 'stepSecond');
-			$strJqOptions .= $this->makeJsProperty('JqTimeFormat', 'timeFormat');
-			$strJqOptions .= $this->makeJsProperty('TimeOnly', 'timeOnly');
-			if ($strJqOptions) $strJqOptions = substr($strJqOptions, 0, -2);
-			return $strJqOptions;
+		static public function jqFrmt($qcFrmt) {
+			return strtr($qcFrmt, self::$mapQC2JQ);
 		}
 
-		public function getJqControlId() {
-			return $this->ControlId;
+		protected function updateDateTimeFormat() {
+			$this->strDateTimeFormat = '';
+			if ($this->strDateFormat) {
+				$this->strDateTimeFormat = $this->strDateFormat;
+			}
+			if ($this->strTimeFormat) {
+				if ($this->strDateTimeFormat)
+					$this->strDateTimeFormat .= ' ';
+				$this->strDateTimeFormat .= $this->strTimeFormat;
+			}
+			// trigger an update to reformat the text with the new format
+			$this->DateTime = $this->dttDateTime;
 		}
 
-		public function getJqSetupFunction() {
-			return 'datetimepicker';
-		}
-
-		public function GetControlJavaScript() {
-			return sprintf('jQuery("#%s").%s({%s})', $this->getJqControlId(), $this->getJqSetupFunction(), $this->makeJqOptions());
-		}
-
-		public function GetEndScript() {
-			return  $this->GetControlJavaScript() . '; ' . parent::GetEndScript();
-		}
-
-		/**
-		 * returns the property name corresponding to the given custom event
-		 * @param QEvent $objEvent the custom event
-		 * @return the property name corresponding to $objEvent
-		 */
-		protected function getCustomEventPropertyName(QEvent $objEvent) {
-			$strEventClass = get_class($objEvent);
-			if (array_key_exists($strEventClass, QJqDateTimePicker::$custom_events))
-				return QJqDateTimePicker::$custom_events[$strEventClass];
-			return parent::getCustomEventPropertyName($objEvent);
-		}
-
+		/////////////////////////
+		// Public Properties: GET
+		/////////////////////////
 		public function __get($strName) {
 			switch ($strName) {
-				case 'AlwaysSetTime': return $this->blnAlwaysSetTime;
-				case 'Ampm': return $this->blnAmpm;
-				case 'Hour': return $this->intHour;
-				case 'HourMin': return $this->intHourMin;
-				case 'HourMax': return $this->intHourMax;
-				case 'Minute': return $this->intMinute;
-				case 'MinuteMin': return $this->intMinuteMin;
-				case 'MinuteMax': return $this->intMinuteMax;
-				case 'Second': return $this->intSecond;
-				case 'SecondMin': return $this->intSecondMin;
-				case 'SecondMax': return $this->intSecondMax;
-				case 'ShowButtonPanel': return $this->blnShowButtonPanel;
-				case 'ShowHour': return $this->blnShowHour;
-				case 'ShowMinute': return $this->blnShowMinute;
-				case 'ShowSecond': return $this->blnShowSecond;
-				case 'ShowTime': return $this->blnShowTime;
-				case 'StepHour': return $this->intStepHour;
-				case 'StepMinute': return $this->intStepMinute;
-				case 'StepSecond': return $this->intStepSecond;
-				case 'JqTimeFormat': return $this->strJqTimeFormat;
-				case 'TimeOnly': return $this->blnTimeOnly;
-				default: 
-					try { 
-						return parent::__get($strName); 
-					} catch (QCallerException $objExc) { 
-						$objExc->IncrementOffset(); 
-						throw $objExc; 
+				// MISC
+				case 'DateFormat': return $this->strDateFormat;
+				case 'TimeFormat': return $this->strTimeFormat;
+
+				default:
+					try {
+						return parent::__get($strName);
+					} catch (QCallerException $objExc) {
+						$objExc->IncrementOffset();
+						throw $objExc;
 					}
 			}
 		}
-
+		/////////////////////////
+		// Public Properties: SET
+		/////////////////////////
 		public function __set($strName, $mixValue) {
 			$this->blnModified = true;
 
 			switch ($strName) {
-				case 'AlwaysSetTime':
+				case 'DateTimeFormat':
+					throw new QCallerException("Please use DateFormat and TimeFormat properties instead of DateTimeFormat");
+
+				case 'JqDateFormat':
 					try {
-						$this->blnAlwaysSetTime = QType::Cast($mixValue, QType::Boolean);
+						$this->strJqDateFormat = QType::Cast($mixValue, QType::String);
+						$this->strDateFormat = QCalendar::qcFrmt($this->strJqDateFormat);
+						$this->updateDateTimeFormat();
 						break;
 					} catch (QInvalidCastException $objExc) {
 						$objExc->IncrementOffset();
 						throw $objExc;
 					}
 
-				case 'Ampm':
+				case 'DateFormat':
 					try {
-						$this->blnAmpm = QType::Cast($mixValue, QType::Boolean);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'Hour':
-					try {
-						$this->intHour = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'HourMin':
-					try {
-						$this->intHourMin = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'HourMax':
-					try {
-						$this->intHourMax = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'Minute':
-					try {
-						$this->intMinute = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'MinuteMin':
-					try {
-						$this->intMinuteMin = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'MinuteMax':
-					try {
-						$this->intMinuteMax = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'Second':
-					try {
-						$this->intSecond = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'SecondMin':
-					try {
-						$this->intSecondMin = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'SecondMax':
-					try {
-						$this->intSecondMax = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'ShowButtonPanel':
-					try {
-						$this->blnShowButtonPanel = QType::Cast($mixValue, QType::Boolean);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'ShowHour':
-					try {
-						$this->blnShowHour = QType::Cast($mixValue, QType::Boolean);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'ShowMinute':
-					try {
-						$this->blnShowMinute = QType::Cast($mixValue, QType::Boolean);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'ShowSecond':
-					try {
-						$this->blnShowSecond = QType::Cast($mixValue, QType::Boolean);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'ShowTime':
-					try {
-						$this->blnShowTime = QType::Cast($mixValue, QType::Boolean);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'StepHour':
-					try {
-						$this->intStepHour = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'StepMinute':
-					try {
-						$this->intStepMinute = QType::Cast($mixValue, QType::Integer);
-						break;
-					} catch (QInvalidCastException $objExc) {
-						$objExc->IncrementOffset();
-						throw $objExc;
-					}
-
-				case 'StepSecond':
-					try {
-						$this->intStepSecond = QType::Cast($mixValue, QType::Integer);
+						$this->strDateFormat = QType::Cast($mixValue, QType::String);
+						$this->strJqDateFormat = QCalendar::jqFrmt($this->strDateFormat);
+						$this->updateDateTimeFormat();
 						break;
 					} catch (QInvalidCastException $objExc) {
 						$objExc->IncrementOffset();
@@ -357,15 +96,19 @@
 				case 'JqTimeFormat':
 					try {
 						$this->strJqTimeFormat = QType::Cast($mixValue, QType::String);
+						$this->strTimeFormat = self::qcFrmt($this->strJqTimeFormat);
+						$this->updateDateTimeFormat();
 						break;
 					} catch (QInvalidCastException $objExc) {
 						$objExc->IncrementOffset();
 						throw $objExc;
 					}
 
-				case 'TimeOnly':
+				case 'TimeFormat':
 					try {
-						$this->blnTimeOnly = QType::Cast($mixValue, QType::Boolean);
+						$this->strTimeFormat = QType::Cast($mixValue, QType::String);
+						$this->strJqTimeFormat = self::jqFrmt($this->strTimeFormat);
+						$this->updateDateTimeFormat();
 						break;
 					} catch (QInvalidCastException $objExc) {
 						$objExc->IncrementOffset();
@@ -375,13 +118,12 @@
 				default:
 					try {
 						parent::__set($strName, $mixValue);
-						break;
 					} catch (QCallerException $objExc) {
 						$objExc->IncrementOffset();
 						throw $objExc;
 					}
+					break;
 			}
 		}
 	}
-
 ?>
--- source/install.php
+++ source/install.php
@@ -3,21 +3,23 @@
 $objPlugin = new QPlugin();
 $objPlugin->strName = "QJqDateTimePicker";
 $objPlugin->strDescription = 'Date time picker control based on <a href="http://trentrichardson.com/examples/timepicker">jQuery date picker plugin</a>.';
-$objPlugin->strVersion = "0.3";
-$objPlugin->strPlatformVersion = "2.0.2";
+$objPlugin->strVersion = "0.4";
+$objPlugin->strPlatformVersion = "2.1.0";
 $objPlugin->strAuthorName = "Vardan Akopian";
 $objPlugin->strAuthorEmail = "vakopian+qcubed [at] gmail [dot] com";
 
 $components = array();
 
-$components[] = new QPluginJsFile("js/jquery-ui-timepicker-addon-0.6.2.min.js");
-$components[] = new QPluginJsFile("js/jquery-ui-timepicker-addon-0.6.2.js");
+$components[] = new QPluginJsFile("js/jquery-ui-timepicker-addon-0.9.9.min.js");
+$components[] = new QPluginJsFile("js/jquery-ui-timepicker-addon-0.9.9.js");
 $components[] = new QPluginCssFile("css");
 
 $components[] = new QPluginControlFile("includes/QJqDateTimePicker.class.php");
 $components[] = new QPluginControlFile("includes/QJqDateTimePickerBase.class.php");
+$components[] = new QPluginControlFile("includes/QJqDateTimePickerGen.class.php");
 $components[] = new QPluginIncludedClass("QJqDateTimePicker", "includes/QJqDateTimePicker.class.php");
 $components[] = new QPluginIncludedClass("QJqDateTimePickerBase", "includes/QJqDateTimePickerBase.class.php");
+$components[] = new QPluginIncludedClass("QJqDateTimePickerGen", "includes/QJqDateTimePickerGen.class.php");
 
 $components[] = new QPluginExample("example/datetimepicker.php", "QJqDateTimePicker: Date time picker control");
 $components[] = new QPluginExampleFile("example/datetimepicker.php");
